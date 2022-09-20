5f0a62553b66358d9dc1eacf0443fb3b82b1998f
Jorditin <76152011+Jorditin@users.noreply.github.com>
2022-09-20 21:17:19 +0200

    Add files via upload

---
+++ 221sumaAB.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$inicio=$_GET("Inicio");
+$final=$_GET("Final");
+$array=array(1,2,3,4,5,6,7,8,9,10);
+$resultado=0;
+for($n=$inicio;$n<=9;$n++){
+    $resultado+=array(n);
+
+}
+echo $resultado;
+
+
+
+?>
+</body>
+</html>
